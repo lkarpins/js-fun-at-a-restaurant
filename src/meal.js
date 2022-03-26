cfce5cecdec64dcb8a4b3b08f22ef45108279a0d
Lidia Karpinski <lidiakarpinski@gmail.com>
2022-03-26 13:16:24 -0400

    Refactored meal tests

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,50 @@
-function nameMenuItem() {
+function nameMenuItem(food) {
+  return `Delicious ${food}`
+};
 
-}
+function createMenuItem(name, price, type) {
+   var menuItem = {
+     name: name,
+     price: price,
+     type: type,
+   }
+    return menuItem;
+};
+
+
+function addIngredients(newIngredient, ingredients) {
+  if (ingredients.includes(newIngredient)) {
+    return ingredients
+  } else {
+    return ingredients.push(newIngredient)
+  }
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
+
+};
+
+function decreasePrice(initialPrice) {
+  var product = (initialPrice * .9)
+  return product;
+};
+
+function createRecipe(title, ingredients, menuItemType){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+return recipe
+};
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
